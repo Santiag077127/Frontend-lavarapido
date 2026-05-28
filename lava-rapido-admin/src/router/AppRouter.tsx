ad995edd59453e5f75c161fcf874aba3ff9b479f
Jose Stiven Perdomo <mrtinez.stivengmail.com>
2026-05-28 11:34:17 -0500

    feat: add login page

--- lava-rapido-admin/src/router/AppRouter.tsx
+++ lava-rapido-admin/src/router/AppRouter.tsx
@@ -2,18 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "../features/auth/pages/LoginPage/LoginPage";
 import { DashboardPage } from "../features/dashboard/pages/DashboardPage";
 import { ServiciosPage } from "../features/dashboard/pages/ServiciosPage";
+import LandingPage from "../features/auth/pages/LandingPage/Landing";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LandingPage />} />        {/* ← Landing */}
+        <Route path="/login" element={<LoginPage />} />     {/* ← Login */}
 
-        {/* Dashboard como layout — Outlet renderiza las rutas hijas */}
         <Route path="/dashboard" element={<DashboardPage />}>
           <Route path="servicios" element={<ServiciosPage />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
